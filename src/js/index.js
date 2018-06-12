b6c71204eac89cc79e25487b8fb59a864d16a7c7
Denis Sychev <denisvsychev@gmail.com>
2018-06-12 16:45:32 +0300

    Вывод на страницу имени и фамилии пользователя
    index.html, index.js, style.css

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { User } from './user';
+
+window.onload = () => {
+    const user = new User('John', 'Connor', 25);
+    const name = user.greeting();
+    const hi = document.getElementById('greetingUser')
+    hi.innerText = name;
+};
---
+++ src/js/user.js
@@ -0,0 +1,17 @@
+'use strict';
+import '../css/style.css';
+
+class User {
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    };
+
+    greeting(){
+        const text = `Добрый день, ${this.firstName} ${this.lastName}`;
+        return text;
+    };
+}
+
+export {User};
