0ef6c006d129119276efb5740522ca629cdb30ff
Denis Sychev <denisvsychev@gmail.com>
2018-06-15 01:13:29 +0300

    Удаление ненужных файлов
    Никогда не будут использоваться в проекте на React

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import { User } from './user';
-
-window.onload = () => {
-    const user = new User('John', 'Connor', 25);
-    const name = user.greeting();
-    const hi = document.getElementById('greetingUser')
-    hi.innerText = name;
-};
