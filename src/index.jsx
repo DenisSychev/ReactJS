7a884833215fdd78a44b91c39c5d4985a8cb1974
Denis Sychev <denisvsychev@gmail.com>
2018-06-26 17:49:14 +0300

    Начало выполнения ДЗ 6
    Пока только по уроку.

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './style.sass';
 
 import Header from 'components/Header';
 
 import NavigationMenu from 'components/NavigationMenu';
-import {navigationMenuItems} from 'components/NavigationMenu/NavigationMenuItems';
+import { navigationMenuItems } from 'components/NavigationMenu/NavigationMenuItems';
 
 import PostsList from 'containers/PostsContainer';
 import UserList from 'containers/UserListContainer';
+import routes from './routes';
 
 class Container extends Component {
     render() {
@@ -16,9 +19,12 @@ class Container extends Component {
             <Fragment>
                 <Header />
                 <NavigationMenu naviMenuItems={navigationMenuItems} />
-                <PostsList />
-                <UserList />
-            </Fragment>            
+                <BrowserRouter>
+                    <Switch>
+                        {routes.map((route) => <Route {...route} />)}
+                    </Switch>
+                </BrowserRouter>
+            </Fragment>
         );
     }
 };
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import UserList from 'containers/UserListContainer';
+
+export default [
+    {
+        path: '/users',
+        component: UserList,
+    },
+]
