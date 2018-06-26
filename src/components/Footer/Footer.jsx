dd9d84bf2dd66a1ca9dcb1b15539dbb64449aabf
Denis Sychev <denisvsychev@gmail.com>
2018-06-27 01:41:50 +0300

    Добавлены подвал и главная страница.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
+import './Footer.sass';
 
 export default class Footer extends PureComponent {
     render() {
         return (
             <div className="footer">
                 <div className="footer-block container">
-                    <p>2018</p>
+                    <p>{(new Date().getFullYear())}</p>
                 </div>
             </div>
         );
