15b4972cf8e51f2d151115da25ac411a6d86d81f
Denis Sychev <denisvsychev@gmail.com>
2018-07-01 17:35:46 +0300

    Добавлено прописывание класса active для активного пункта меню

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -1,19 +1,22 @@
 import React, { PureComponent } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import './NavigationMenu.sass';
+import classNames from 'classnames';
 
-export default class NavigationMenu extends PureComponent {
+
+class NavigationMenu extends PureComponent {
     render() {
+        const {location} = this.props;
         return (
-            <BrowserRouter>
-                <header>
-                    <ul className="navi container">
-                        <li><Link to="/">Обложка</Link></li>
-                        <li><Link to="/users">Пользователи</Link></li>
-                        <li><Link to="/posts">Истории и комментарии</Link></li>
-                    </ul>
-                </header>
-            </BrowserRouter>
+            <header>
+                <ul className="navi container">
+                    <li className={classNames({ active:location.pathname === '/' })}><Link to="/">Обложка</Link></li>
+                    <li className={classNames({ active:location.pathname === '/users' })}><Link to="/users">Пользователи</Link></li>
+                    <li className={classNames({ active:location.pathname === '/posts' })}><Link to="/posts">Истории и комментарии</Link></li>
+                </ul>
+            </header>
         );
     }
 };
+
+export default withRouter(NavigationMenu);
