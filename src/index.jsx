c3dc595297868bfc8e8958c6ee444463c3e270c6
Denis Sychev <denisvsychev@gmail.com>
2018-07-01 17:33:58 +0300

    Меню навигации убрано из index. Добавлен key, что бы убрать стандартный варнинг

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import './style.sass';
-import 'components/NavigationMenu/NavigationMenu.sass';
 
 import Header from 'components/Header';
+import NavigationMenu from 'components/NavigationMenu';
 import Footer from 'components/Footer';
 
 import routes from './routes';
@@ -17,15 +17,9 @@ class Container extends Component {
                 <Header />
                 <BrowserRouter>
                 <Fragment>
-                    <header>
-                        <ul className="navi container">
-                            <li><Link to="/">Обложка</Link></li>
-                            <li><Link to="/users">Пользователи</Link></li>
-                            <li><Link to="/posts">Истории и комментарии</Link></li>
-                        </ul>
-                    </header>
+                    <NavigationMenu />
                     <Switch>
-                        {routes.map((route) => <Route {...route} />)}
+                        {routes.map((route, idx) => <Route key={idx} {...route} />)}
                     </Switch>
                     </Fragment>
                 </BrowserRouter>
